792ede06d0f4a26cd633277cfa9ba3287a9fe4fb
DylanCE <166676498+MocaPan@users.noreply.github.com>
2025-03-18 19:36:52 -0600

    [ADD] los métodos de subscription

--- MQBroker/Subscription.cs
+++ MQBroker/Subscription.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace MQBroker
 {
-    class Subscription
+    public class Subscription
     {
+        public string AppID { get; private set; }
+        public string Topic { get; private set; }
+
+        public Subscription(string appID, string topic)
+        {
+            this.AppID = appID;
+            this.Topic = topic;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Subscription other)
+            {
+                return this.AppID.Equals(other.AppID, StringComparison.OrdinalIgnoreCase) &&
+                       this.Topic.Equals(other.Topic, StringComparison.OrdinalIgnoreCase);
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return (AppID + Topic).GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"AppID: {AppID}, Topic: {Topic}";
+        }
     }
 }
+
