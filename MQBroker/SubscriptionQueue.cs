a992556d3b11efc2326d0a101b83199d05d050c2
DylanCE <166676498+MocaPan@users.noreply.github.com>
2025-03-21 11:43:57 -0600

    [ADD] CLass SubscriptionQueue

---
+++ MQBroker/SubscriptionQueue.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MQBroker
+{
+    /// Asocia una suscripción (AppID, Topic) con una cola de mensajes (MessageLinkedList).
+    public class SubscriptionQueue
+    {
+        public Subscription Subscription { get; set; }
+        public MessageLinkedList Messages { get; set; }
+
+        public SubscriptionQueue(Subscription subscription)
+        {
+            Subscription = subscription;
+            Messages = new MessageLinkedList();
+        }
+
+        // Representación de la cantidad de mensajes en cola para un un usuario, para debugging o logs.
+        public override string ToString()
+        {
+            return $"{Subscription.ToString()}, Mensajes en cola: {Messages.Count}";
+        }
+    }
+}
